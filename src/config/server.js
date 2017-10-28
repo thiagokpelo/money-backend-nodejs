90ecdd55bdfb488ff781db433e34bc9a0b9da471
Thiago Capelo <thiagokpelo@gmail.com>
2017-10-28 20:44:13 -0200

    Subinto conts port

--- src/config/server.js
+++ src/config/server.js
@@ -1,10 +1,11 @@
+const port        = 80
+
 const bodyParser  = require('body-parser')
 const queryParser = require('express-query-int')
 const express     = require('express')
 const server      = express()
 
 const allowCors   = require('./cors')
-const port        = 80
 
 server.use( bodyParser.urlencoded({ extended: true }) )
 server.use( bodyParser.json() )
